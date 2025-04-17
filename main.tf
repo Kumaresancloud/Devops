25f775f4ef006bac0c5d294986f1da3c004e5ca9
Kumaresancloud <gkumaresan.mca@gmail.com>
2025-04-17 11:25:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,13 @@
 
 # Create SNS Topic in Account A
 resource "aws_sns_topic" "sns_topic" {
+  provider = aws.account_a
   name = "cross-account-topic"
 }
 
 # IAM Role for EC2 to publish to SNS
 resource "aws_iam_role" "ec2_publish_role" {
+  provider = aws.account_a
   name = "ec2-publish-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -22,27 +24,59 @@ resource "aws_iam_role" "ec2_publish_role" {
 }
 
 resource "aws_iam_policy" "sns_publish_policy" {
+  provider = aws.account_a
   name = "sns-publish-policy"
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = ["sns:Publish"],
-      Resource = aws_sns_topic.sns_topic.arn
-    }]
+    "Version": "2008-10-17",
+    "Id": "SNS",
+    "Statement": [
+      {
+        "Sid": "__default_statement_ID",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "*"
+        },
+        "Action": [
+          "SNS:Publish",
+          "SNS:RemovePermission",
+          "SNS:SetTopicAttributes",
+          "SNS:DeleteTopic",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:GetTopicAttributes",
+          "SNS:AddPermission",
+          "SNS:Subscribe"
+        ],
+        "Resource": aws_sns_topic.sns_topic.arn,
+        "Condition": {
+          "StringEquals": {
+            "AWS:SourceOwner": "541153896426"
+          }
+        }
+      },
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::568812757521:root"
+        },
+        "Action": "sns:Subscribe",
+        "Resource": aws_sns_topic.sns_topic.arn
+      }
+    ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "sns_policy_attachment" {
+  provider = aws.account_a
   role       = aws_iam_role.ec2_publish_role.name
   policy_arn = aws_iam_policy.sns_publish_policy.arn
 }
 
 # EC2 Instance in Account A
 resource "aws_instance" "ec2_publisher" {
-  ami                         = "ami-xxxxxxxx" # Replace with a valid AMI
+  provider = aws.account_a
+  ami                         = "ami-00a929b66ed6e0de6" # Replace with a valid AMI
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-xxxxxxxx" # Replace accordingly
+  subnet_id                   = "subnet-0d3ba75070ff8198b" # Replace accordingly
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
   user_data = <<-EOF
@@ -50,11 +84,12 @@ resource "aws_instance" "ec2_publisher" {
               yum install -y aws-cli jq
               aws sns publish --topic-arn ${aws_sns_topic.sns_topic.arn} \
                 --message "Hello from Account A EC2" \
-                --region ${var.region}
+                --region us-east-1
               EOF
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
+  provider = aws.account_a
   name = "ec2-profile"
   role = aws_iam_role.ec2_publish_role.name
 }
@@ -65,22 +100,24 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 # Create SQS Queue
 resource "aws_sqs_queue" "sqs_queue" {
+  provider = aws.account_b
   name = "cross-account-queue"
 }
 
 # Policy to allow SNS from Account A to publish
 resource "aws_sqs_queue_policy" "sqs_policy" {
+  provider = aws.account_b
   queue_url = aws_sqs_queue.sqs_queue.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      Principal = { AWS = "arn:aws:iam::<ACCOUNT_A_ID>:root" },
+      Principal = { AWS = "arn:aws:iam::541153896426:root" },
       Action = "sqs:SendMessage",
       Resource = aws_sqs_queue.sqs_queue.arn,
       Condition = {
         ArnEquals = {
-          "aws:SourceArn" = "arn:aws:sns:${var.region}:<ACCOUNT_A_ID>:cross-account-topic"
+          "aws:SourceArn" = "arn:aws:sns:us-east-1:541153896426:cross-account-topic"
         }
       }
     }]
@@ -89,6 +126,7 @@ resource "aws_sqs_queue_policy" "sqs_policy" {
 
 # EC2 Role to Read from SQS
 resource "aws_iam_role" "ec2_consume_role" {
+  provider = aws.account_b
   name = "ec2-consume-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -102,6 +140,7 @@ resource "aws_iam_role" "ec2_consume_role" {
 }
 
 resource "aws_iam_policy" "sqs_receive_policy" {
+  provider = aws.account_b
   name = "sqs-receive-policy"
   policy = jsonencode({
     Version = "2012-10-17",
@@ -120,15 +159,17 @@ resource "aws_iam_policy" "sqs_receive_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "sqs_policy_attachment" {
+  provider = aws.account_b
   role       = aws_iam_role.ec2_consume_role.name
   policy_arn = aws_iam_policy.sqs_receive_policy.arn
 }
 
 # EC2 Instance in Account B
 resource "aws_instance" "ec2_consumer" {
-  ami                         = "ami-yyyyyyyy" # Replace with valid AMI
+  provider = aws.account_b
+  ami                         = "ami-00a929b66ed6e0de6" # AMI for ec2
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-yyyyyyyy" # Replace accordingly
+  subnet_id                   = "subnet-1e767854" # subnetID
   iam_instance_profile        = aws_iam_instance_profile.consumer_profile.name
   associate_public_ip_address = true
   user_data = <<-EOF
@@ -148,6 +189,7 @@ resource "aws_instance" "ec2_consumer" {
 }
 
 resource "aws_iam_instance_profile" "consumer_profile" {
+  provider = aws.account_b
   name = "consumer-profile"
   role = aws_iam_role.ec2_consume_role.name
 }
