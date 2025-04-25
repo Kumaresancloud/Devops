7c2adc1ad3097c83420c39a8f356ffe97bf6cbaf
Kumaresancloud <gkumaresan.mca@gmail.com>
2025-04-25 14:27:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+variable "account_a_id" {}
+variable "account_b_id" {}
+
+variable "account_a_subnet_id" {}
+variable "account_b_subnet_id" {}
+
 module "account_a_sns_publisher" {
   source    = "./modules/account_a"
   providers = {
     aws = aws.account_a
   }
-  subnet_id       = "subnet-xxxxxxxx"
-  account_b_id    = "ACCOUNT_B_ID"
+  subnet_id    = var.account_a_subnet_id
+  account_b_id = var.account_b_id
 }
 
 module "account_b_sqs_consumer" {
@@ -12,6 +18,6 @@ module "account_b_sqs_consumer" {
   providers = {
     aws = aws.account_b
   }
-  subnet_id    = "subnet-yyyyyyyy"
-  account_a_id = "ACCOUNT_A_ID"
+  subnet_id    = var.account_b_subnet_id
+  account_a_id = var.account_a_id
 }
