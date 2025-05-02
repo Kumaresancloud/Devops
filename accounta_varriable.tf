9104c3ce00521061a83c943ea405469e556a80fe
Kumaresancloud <gkumaresan.mca@gmail.com>
2025-05-02 01:53:16 -0400

    Update accounta_varriable.tf

--- accounta_varriable.tf
+++ accounta_varriable.tf
@@ -1,9 +1,1 @@
-variable "subnet_id" {
-  description = "The subnet ID for the EC2 instance"
-  type        = string
-}
-
-variable "account_b_id" {
-  description = "The AWS Account ID of Account B (for cross-account access)"
-  type        = string
-}
+variable "sg_name" {}
